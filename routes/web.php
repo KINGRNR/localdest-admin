11dfc663ec90313753b2c818496fb68660c4463b
Rafif <rafifnr06@gmail.com>
2024-05-30 20:57:57 +0700

    simplify web.php

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,20 @@
 <?php
 
-use App\Http\Controllers\ListdestinationController;
-use App\Http\Controllers\ListuserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('dashboard.index');
-// })->middleware(['auth', 'verified']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/dashboard', function () {
-    return view('dashboard.index');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/manage-user', function () {
-        return view('manage-user.index');
-    });
-    Route::get('/manage-destination', function () {
-        return view('manage-destination.index');
-    });
-    Route::controller(ListuserController::class)->group(function () {
-        foreach (['index', 'show', 'create', 'update', 'delete', 'getData', 'savesuspend', 'deleteUser'] as $key => $value) {
-            Route::post('/listuser/' . $value, $value);
-        }
-    });
-    Route::controller(ListdestinationController::class)->group(function () {
-        foreach (['index', 'show', 'create', 'update', 'delete', 'getData', 'savesuspend', 'deleteUser'] as $key => $value) {
-            Route::post('/listdest/' . $value, $value);
-        }
-    });
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
